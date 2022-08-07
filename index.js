3b669f3f788015c7024915dc08aee0c11d4cdacf
uFeat <98409133+Amne-Dev@users.noreply.github.com>
2022-08-07 12:00:53 +0200

    Test 1 slider

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
+var slider  = document.getElementById('slider')
+var active  = document.getElementById('active')
+var line1  = document.getElementById('line1')
+var line2  = document.getElementById('line2')
+var line3  = document.getElementById('line3')
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
+line1.onclick = function(){
+  slider.style.transform = 'translateX(0)'
+}
+line2.onclick = function(){
+  slider.style.transform = 'translateX(-25%)'
+}
+line3.onclick = function(){
+  slider.style.transform = 'translateX(-50%)'
+}
